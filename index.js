3f48567abc92148c5f70b459ce404ee22c0a5e7d
Vladyslav <kytsevolov@gmail.com>
2020-08-21 19:28:52 -0600

    sum array recursively

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let result = 0;
+  array.forEach((el) => {
+    if (Array.isArray(el)) {
+      result += sumItems(el);
+    } else {
+      if (array.length) {
+        result += el;
+      }
+    }
+  });
+  return result;
 }
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 
 module.exports = sumItems;
